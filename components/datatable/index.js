0d58f7de0971221e092cfa44a6c5d0da52a55ed8
Unai Perez <unaipme@gmail.com>
2021-10-28 16:19:08 +0200

    Tables are now sortable

--- components/datatable/index.js
+++ components/datatable/index.js
@@ -1,18 +1,38 @@
-import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, TablePagination, IconButton } from "@mui/material";
-import { useRouter } from "next/router";
+import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, TablePagination, IconButton, TableSortLabel } from "@mui/material";
+import { visuallyHidden } from '@mui/utils';
 import { useState } from "react";
 
 import entities from "../../public/schema";
 
 import EditIcon from '@mui/icons-material/Edit';
+import { Box } from "@mui/system";
 
 const DataTable = ({ entity, data }) => {
     const [ page, setPage ] = useState(0);
     const [ rowsPerPage, setRowsPerPage ] = useState(5);
 
     const { fields } = entities[entity];
+    const [ orderBy, setOrderBy ] = useState(fields.includes("id") ? "id" : fields[0]);
+    const [ order, setOrder ] = useState("asc", "desc");
 
-    const router = useRouter();
+    const handleRequestSort = (property) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    }
+
+    const descendingComparator = (a, b) => {
+        if (b[orderBy] < a[orderBy]) {
+          return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+          return 1;
+        }
+        return 0;
+    }
+
+    const getComparator = () => 
+        order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
 
     return (
         <>
@@ -28,13 +48,28 @@ const DataTable = ({ entity, data }) => {
                     <TableHead>
                         <TableRow>
                             {fields.map((name, index) => (
-                                <TableCell key={index} align={index === 0 ? "left" : "right"}>{name}</TableCell>
+                                <TableCell key={index}
+                                           sortDirection={orderBy === name ? order : false}
+                                           align={index === 0 ? "left" : "right"}>
+                                                <TableSortLabel active={orderBy === name}
+                                                                direction={orderBy === name ? order : "asc"}
+                                                                onClick={() => handleRequestSort(name)}
+                                                                style={{ color: "white" }}>
+                                                    {name}
+                                                    {orderBy === name &&
+                                                        <Box component="span" sx={ visuallyHidden }>
+                                                            {order === "desc" ? "sorted descending" : "sorted ascending"}
+                                                        </Box>
+                                                    }
+                                                </TableSortLabel>
+                                </TableCell>
                             ))}
                             <TableCell>Edit</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .sort(getComparator(order, orderBy))
                             .map(row => (
                             <TableRow key={row.id}>
                                 {fields.map((name, index) => (
