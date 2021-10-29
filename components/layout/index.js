25cff7a8bb738a3c729ccc9df0d5851ae6c69ef3
Unai Perez <unaipme@gmail.com>
2021-10-29 10:31:32 +0200

    Now sidebar elements highlight according to current URL

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,19 +1,21 @@
-import styles from "./layout.module.css";
+import styles from "./layout.module.scss";
 import entities from "../../public/schema";
 
 import Link from "next/link";
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import pluralize from "pluralize";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 import DashboardIcon from '@mui/icons-material/Dashboard';
 
 const title = (str) => str.slice(0, 1).toUpperCase().concat(str.slice(1));
 
-const PageEntry = ({ name, url, Icon }) => {
+const PageEntry = ({ name, url, Icon, selected }) => {
     return (
         <Link href={url}>
-            <div className={styles.page_entry}>
+            <div className={selected === true ? styles.selected_page_entry : styles.page_entry}>
                 { !!Icon && <div> <Icon /> </div> }
                 <div>{ title(name) }</div>
             </div>
@@ -22,13 +24,25 @@ const PageEntry = ({ name, url, Icon }) => {
 }
 
 const Layout = ({ children }) => {
+    const router = useRouter();
+    const { entity } = router.query;
+    const [ selectedEntity, setSelectedEntity ] = useState(entity);
+
+    useEffect(() => {
+        setSelectedEntity(entity);
+    }, [entity]);
+
     return (
         <Grid container spacing={2} className={styles.layout}>
             <Grid item xl={3} className={styles.layout_page_column}>
                 <PageEntry url="/" name="Dashboard" Icon={DashboardIcon} />
                 <Divider />
                 {Object.entries(entities).map(([name, { Icon }], index) =>
-                    <PageEntry key={index} url={`/${name}`} name={pluralize(name)} Icon={Icon} />
+                    <PageEntry key={index}
+                               url={`/${name}`}
+                               name={pluralize(name)}
+                               Icon={Icon}
+                               selected={selectedEntity === name} />
                 )}
             </Grid>
             <Grid item xl={9} className={styles.layout_content_column}>
